6223290ce2bcdf6c2467762d330af114126f7695
Avinash Nandyala <42831255+navinash47@users.noreply.github.com>
2021-01-31 22:43:47 +0530

    Update functions_for_dashboard.py

--- NewApp/flask/app/functions_for_dashboard.py
+++ NewApp/flask/app/functions_for_dashboard.py
@@ -1,14 +1,15 @@
 def plugin_csv(name,phone_no,from_date,to_date,file_name):
+
     import pandas as pd
     import numpy as ny
+    #take data from influx db
     df=pd.read_csv('dummy.csv')
     from datetime import date, timedelta
     test=[]
     test.append(phone_no)
     df1=df.loc[df['Phone']==ny.int64(phone_no)]
-    # df1=df
-    # from_date="2021-01-25"
-    # to_date="2021-01-27"
+    
+    #
     f_date_split=from_date.split("-")
     t_date_split=to_date.split("-")
     sdate = date(int(f_date_split[0]), int(f_date_split[1]), int(f_date_split[2]))   # start date
@@ -20,9 +21,24 @@ def plugin_csv(name,phone_no,from_date,to_date,file_name):
     for i in range(delta.days + 1):
         day = sdate + timedelta(days=i)
         days.append(str(day))
-        dd=dd+"<>"+str(day)
+    
     df1=df1.loc[df['time'].isin(days)]
     path="app/static/"+file_name
     path_file=r"{}".format(path)
     df1.to_csv(path_file,index=False)
-    return dd+str(len(df1))+str(len(df))+phone_no
+    return "Working"
+
+
+def plugin_check_cred(name,phone_no):
+    import pandas as pd
+    import numpy as ny
+    #take data from influx db
+    df = pd.read_csv("dummy.csv")
+
+    df=df.loc[df["Phone"]==ny.int64(phone_no)]
+    df=df.loc[df["Name"]==name]
+
+    if(len(df)==0):
+        return False
+    else:
+        return True
